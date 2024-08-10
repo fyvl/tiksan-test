30933f5aafbaf48d77af13cc990a973f4f7dd9e9
fyvl <yfyl77ov@gmail.com>
2024-08-10 11:58:02 +0700

    initial commit

---
+++ resources/js/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+	<div class="container">
+		Hello world!
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	.container {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+</style>
